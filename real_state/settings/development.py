3b63e778908ed2efab7c98a62d130a4ceef0b215
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2024-01-02 17:06:28 -0600

    djoser settings user api

--- real_state/settings/development.py
+++ real_state/settings/development.py
@@ -1,6 +1,17 @@
 from .base import *
 
 
+EMAIL_BACKEND = "django.core.email.backends.smtp.EmailBackend"
+
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@real-estate.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
+
 # To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres. mariachonjunco@gmail.com
 
 DATABASES = {
