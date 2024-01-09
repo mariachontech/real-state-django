f1075ea5d23a84423a6c780ef3f7246c7931e145
Jose Juan Tellez Guzman <josstell@Joses-MacBook-Air.local>
2024-01-09 16:35:40 -0600

    register, login, reset password user

--- real_state/settings/development.py
+++ real_state/settings/development.py
@@ -1,14 +1,17 @@
 from .base import *
 
 
-EMAIL_BACKEND = "django.core.email.backends.smtp.EmailBackend"
+# EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_BACKEND = "apps.users.backend.email_backend.EmailBackend"
+
 
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_USE_TLS = True
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = "info@real-estate.com"
+DEFAULT_FROM_EMAIL = "info@real-estate.mariachon.com"
 DOMAIN = env("DOMAIN")
 SITE_NAME = "Real Estate"
 
